f9c6f6658aea6366ceea0b87a224f281762382f9
hyunho <mizzking75@gmail.com>
2023-01-01 00:15:38 +0900

    Initial Commit

---
+++ BamiLog/BamiLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BamiLogApp.swift
+//  BamiLog
+//
+//  Created by hyunho lee on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct BamiLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
