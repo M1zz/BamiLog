00854de712cc8c05733e9e50acd3d83ff75855a4
steady-on <whitebsm93@naver.com>
2023-01-14 03:36:14 +0900

    feat : Apart UIScreen extension of ContentView
    - ContentView에서 UIScreen extension 분리

--- BamiLog/View/ContentView.swift
+++ BamiLog/View/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
     @State var isEnterProfile: Bool = false
     @State var isBathTimerShow: Bool = false
     @State var buttonType: ButtonType?
-    private let cellWidth = UIScreen.screenWidth/2 - 30
+
     var body: some View {
         VStack {
             // MARK: Header
@@ -198,10 +198,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(profile: Profile(name: "테스트", birthDate: Date()), buttonType: .milk)
     }
-}
-
-extension UIScreen {
-    static let screenWidth = UIScreen.main.bounds.size.width
-    static let screenHeight = UIScreen.main.bounds.size.height
-    static let screenSize = UIScreen.main.bounds.size
 }
